80aaec8303003ffcbfb9789f201fb698956b8e67
feras ammouri <m.feras.ammouri@gmail.com>
2020-05-08 15:04:41 +0200

    Partially completed Banking microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext _bankingDbContext;
+
+        public AccountRepository(BankingDbContext bankingDbContext)
+        {
+            _bankingDbContext = bankingDbContext;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _bankingDbContext.Accounts;
+        }
+    }
+}
